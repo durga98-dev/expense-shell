ec28a1eff46ee0dbadfb14b7e2312ae8277e6d23
Kapavarapu <durgadevi.kapavarapu@accenture.com>
2025-06-30 15:36:47 +0530

    committing 14dayslogs script

---
+++ 14dayslogs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOG_FOLDER=$(sudo mkdir -p /var/log/expense-logs)
+LOG_FILE=$(echo $0|cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+        then
+            echo -e "$2.... is $G SUCCESS $N"
+        else
+            echo -e "$2.... is $R FAILED $N"
+            exit 1
+    fi
+}
+
+#Delete 14days older files
+
+DIR="/home/ec2-user/app-logs"
+OLD=$(find $DIR -name "*.log" -mtime +14)
+echo -e "$R Files to be deleted are $N: $OLD"
+
+ 
